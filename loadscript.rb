38e474c4f545a454e72950a1bdd6fbfa67987b6c
hennes131 <hennes131@googlemail.com>
2017-03-27 13:28:39 +0200

    funzt

--- loadscript.rb
+++ loadscript.rb
@@ -1,25 +1,34 @@
 require 'faker'
 require 'pg'
 
-db = PG.connect( {:dbname => 'g16', :user => 'gerhardt', :password => '%G16Admin', :host => '172.16.161.82'} )
+db = PG.connect( {:dbname => 'g16', :user => 'gerhardt', :password => '%G16Admin', :host => 'localhost'} )
 
 i = 2
 
-while i < 100
-	first_name = Faker::Name.unique.first_name
-	last_name = Faker::Name.unique.last_name
-	name = first_name+" "+last_name   
+while i < 10000
+	if i % 100 == 0 
+		Faker::Name.unique.clear
+		Faker::Internet.unique.clear
+		Faker::Address.unique.clear
+	end
+	first_name1 = Faker::Name.unique.first_name.tr("'","`")
+	first_name = %Q(#{first_name1})
+	last_name1 = Faker::Name.unique.last_name.tr("'","`")
+	last_name = %Q(#{last_name1})
+	name = first_name+" "+last_name 
 	email = Faker::Internet.unique.email(name)
-	address = Faker::Address.unique.street_address
-	city = Faker::Address.unique.city
-	zip = Faker::Address.unique.zip
+	address1 = Faker::Address.unique.street_address.tr("'","`")
+	address = %Q(#{address1})
+	city1 = Faker::Address.unique.city.gsub!("'","`")
+	city = %Q(#{city1})
+	zip = Faker::Address.unique.zip[0..4]
 	gender = ["m", "w"].sample
 	i = i+1
+
+
+#puts zip
+	sql = "INSERT INTO customers (first_name, last_name, address, zip, city, gender, email) 
+		VALUES('#{first_name}', '#{last_name}', '#{address}', '#{zip}', '#{city}', '#{gender}', '#{email}');"
+
+	db.exec(sql)
 end
-
-
-sql = "INSERT INTO customers (first_name, last_name, address, zip, city, gender, email) 
-		#VALUES(#{first_name}, #{last_name}, #{address}, #{zip}, #{city}, #{email})"
-
-db.exec(sql)
-
